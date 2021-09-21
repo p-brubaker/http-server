abe7d030a8540de4a8411fc76fdcfca6c25d328f
Paul Brubaker <pbrubak@gmail.com>
2021-09-20 20:10:59 -0700

    passing tests for 404 from GET /styles and GET /bad-file

--- tests/server.test.js
+++ tests/server.test.js
@@ -1,6 +1,7 @@
 import app from '../lib/app.js';
 import request from 'supertest';
 import { readFile } from 'fs/promises';
+import basicHtml from '../lib/utils/basicHtml.js';
 
 describe('Serve static files from /public', () => {
     it('should return index.html from GET /', async () => {
@@ -15,5 +16,17 @@ describe('Serve static files from /public', () => {
         const text = res.text;
         const css = await readFile('public/styles/main.css', 'utf-8');
         expect(text).toEqual(css);
+    });
+
+    it('should return 404 from GET /bad-file', async () => {
+        const res = await request(app).get('/bad-file');
+        const text = res.text;
+        expect(text).toEqual(basicHtml('<h1>Not Found</h1>'));
+    });
+
+    it('should return 404 from GET /styles', async () => {
+        const res = await request(app).get('/styles');
+        const text = res.text;
+        expect(text).toEqual(basicHtml('<h1>Not Found</h1>'));
     });
 });
