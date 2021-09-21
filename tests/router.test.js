5e07b3c73936e5adfde8690c8aeee49a64702400
Paul Brubaker <pbrubak@gmail.com>
2021-09-21 01:02:48 -0700

    passing test for router GET and POST /cats and /cats:id routes

--- tests/router.test.js
+++ tests/router.test.js
@@ -3,10 +3,9 @@ import request from 'supertest';
 
 describe('Resource Router', () => {
     it('should match POST /cats and GET /cats/:id', async () => {
-        const fakeCatObject = { 'name': 'furball', 'age': 13 };
-        const postCatsResponse = await request(app).post('/cats', {
-            body: fakeCatObject
-        });
+        const postCatsResponse = await request(app).post('/cats').send((
+            { 'name': 'furball', 'age': 13 }
+        ));
         expect(postCatsResponse.body).toEqual({
             message: 'cat posted successfully',
             id: expect.any(String)
@@ -14,8 +13,8 @@ describe('Resource Router', () => {
         const catId = postCatsResponse.body.id;
 
         const getCatResponse = await request(app).get(`/cats/${catId}`);
-        expect(getCatResponse).toEqual(
-            { 'name': 'furball', 'age': 13 }
+        expect(JSON.parse(getCatResponse.body)).toEqual(
+            { 'name': 'furball', 'age': 13, id: expect.any(String) }
         );
     });
 });
