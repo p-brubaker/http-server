09d2293668b105e70691cff0d5b3e88f7a862b9e
Paul Brubaker <pbrubak@gmail.com>
2021-09-20 23:18:25 -0700

    failing test for bodyParser returns deserialized body

--- tests/parser.test.js
+++ tests/parser.test.js
@@ -11,4 +11,20 @@ describe('Body Parser takes a req obj and returns a promise for the pased body',
         const request = { method: 'POST', url: '/cats', contentType: 'text/html' };
         expect(await bodyParser(request)).toEqual('400 Bad Request');
     });
+
+    it('should return deserialized body from req emitted events (using JSON.parse())', async () => {
+        const request = { 
+            method: 'POST', 
+            url: '/cats', 
+            contentType: 'application/json',
+            // eslint-disable-next-line quotes
+            body: "[{'name': 'furball'}, {'name': 'other cat'}]"
+        };
+        const parsedBody = [
+            { 'name': 'furball' },
+            { 'name': 'other cat' }
+        ];
+        const result = await bodyParser(request);
+        expect(result).toEqual(parsedBody);
+    });
 });
